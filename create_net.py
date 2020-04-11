457043785ddedf8df9ee929ae8c967b00b3aae02
Elie KADOCHE <eliekadoche78@gmail.com>
2020-04-11 13:32:18 +0200

    Typo

--- create_net.py
+++ create_net.py
@@ -5,18 +5,23 @@ import torch
 import utils
 
 if __name__ == "__main__":
+    # Parser
+    # ---------------
     parser = argparse.ArgumentParser("Script to create a neural net")
     parser.add_argument("--cuda", help="If using GPU", action="store_true")
     parser.add_argument("--path", help="Model path",
                         type=str, default="networks/neural_net.pt")
 
     # Parse argument
+    # ---------------
     args = parser.parse_args()
 
     # Using CUDA if asked and available
+    # ---------------
     use_cuda = args.cuda and torch.cuda_is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
     # Creating and saving the neural net
+    # ---------------
     model = utils.Net().to(device)
     torch.save(model, args.path)
